ba45e063bae70944438622e63db8094fc009d524
Z-Coder10 <adetomiwaomololu@gmail.com>
2024-12-11 07:49:46 -1100

    123456789

---
+++ Triangle.py
@@ -0,0 +1,13 @@
+import turtle
+turtle.Screen().bgcolor("green")
+board = turtle.Turtle()
+board.forward(100)
+board.left(120)
+board.forward(150)
+board.left(120)
+board.forward(150)
+
+board.penup()
+board.right(150)
+board.left(120)
+board.forward(100)      
