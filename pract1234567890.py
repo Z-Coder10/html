dbea29e69702c6315373dad6afe24b8f0f93c8b1
Z-Coder10 <adetomiwaomololu@gmail.com>
2024-12-18 07:46:06 -1100

    1234567890-

---
+++ pract1234567890.py
@@ -0,0 +1,43 @@
+lst = ['redd', ' blue', 'red', 'green']
+
+# for x in lst:
+
+# print(x)
+
+# name= "Tharshan"
+
+# for char in name:
+
+# print(char)
+
+# for i in range(0, 11):
+
+# print(i)
+
+lst = ['redd', ' blue', 'red', 'green']
+
+names= ['Zakir', 'Tharshan', 'David', 'Fareed']
+
+for x in lst:
+
+for y in names:
+
+print(x, y)
+
+i = 0
+
+while i < 6:
+
+print(i)
+
+i += 1
+
+num = 17
+
+print ("Table of 17 \n")
+
+for i in range(1, 11):
+
+x = num *i
+
+print('17 x %d = %d' % (i,x))
