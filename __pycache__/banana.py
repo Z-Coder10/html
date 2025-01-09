a0c1ac2aaaf81cf0c4efc89592a58f857a7b3755
Z-Coder10 <adetomiwaomololu@gmail.com>
2025-01-09 07:53:35 -1100

    1234567890-

---
+++ __pycache__/banana.py
@@ -0,0 +1,11 @@
+print("Star pattern \n")
+
+for i in range(1,11):
+    
+    for j in range(i):
+        
+        print("*",end = "")
+        
+    print("\n")
+        
+print("Invert Star Pattern \n")
