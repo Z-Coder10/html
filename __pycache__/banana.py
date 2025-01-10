484820a5b1739885c6b30a8ce88f97a050730da8
Z-Coder10 <adetomiwaomololu@gmail.com>
2025-01-10 05:25:45 -1100

    object oriented programming

--- __pycache__/banana.py
+++ __pycache__/banana.py
@@ -1,11 +1,20 @@
-print("Star pattern \n")
+# print("Star pattern \n")
 
-for i in range(1,11):
+# for i in range(1,11):
     
-    for j in range(i):
+#     for j in range(i):
         
-        print("*",end = "")
+#         print("*",end = "")
         
-    print("\n")
+#     print("\n")
         
-print("Invert Star Pattern \n")
+# print("Invert Star Pattern \n")
+class student:
+    stream = "Css"
+    def __init__(self, rom):
+         self.rom = rom
+    def address(self,address):
+        self.address = address
+add = student(10)
+add.address("CAMBRIDGE")
+print(add.address)
