030662c9035baf777a1709034c3356c80b9d37c9
Z-Coder10 <adetomiwaomololu@gmail.com>
2025-03-26 07:53:28 -1100

    guess the number

---
+++ __pycache__/guess.py
@@ -0,0 +1,18 @@
+actual_cost = float(input("please enter actual product cost"))
+sale_amount = float(input("Please enter the sales amount"))
+
+if(sale_amount >actual_cost):
+    profit = sale_amount - actual_cost
+    print("Total profit = {0}".format(profit))
+    
+else:
+    print("Oops you got loss")
+num = int(input("Please enter a number to compare whether it is greater than 15."))
+if(num  >15):
+    print("The number is greater than 15")
+    
+elif(num == 15):
+    print("The number is equal to 15")
+        
+else:
+    print("The number is lesser than 15")
