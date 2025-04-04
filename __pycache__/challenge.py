0947bc7b7fc2ca91ef3c873c41daf502e1f47e00
Z-Coder10 <adetomiwaomololu@gmail.com>
2025-04-04 07:27:42 -1100

    challenge

---
+++ __pycache__/challenge.py
@@ -0,0 +1,9 @@
+print("enter a number(Numerator): ")
+num1= int(input())
+print("Enter a number(denominator): ")
+num2 = int(input())
+
+if num1 % num2 ==0:
+    print("\n" + str(num1) + " is divisible by " + str(num2))
+else:
+    print('\n' + str(num1) + " is not divisible by ")
