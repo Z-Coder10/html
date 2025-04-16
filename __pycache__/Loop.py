d4127dbaf9816371be362370f246ebcacf41924c
Z-Coder10 <adetomiwaomololu@gmail.com>
2025-04-16 20:03:44 +0100

    Loop

---
+++ __pycache__/Loop.py
@@ -0,0 +1,25 @@
+num=int(input("Enter the number: "))
+
+sum =0
+
+i=1
+
+while i<=num:
+
+sum=sum + i
+
+i=i+1
+
+print("\nsum =",sum)num=int(input("Enter the number: "))
+
+sum =0
+
+i=1
+
+while i<=num:
+
+sum=sum + i
+
+i=i+1
+
+print("\nsum =",sum)
--- __pycache__/guess.py
+++ __pycache__/guess.py
@@ -1,12 +1,14 @@
 actual_cost = float(input("please enter actual product cost"))
 sale_amount = float(input("Please enter the sales amount"))
 
+loss = actual_cost - sale_amount
+
 if(sale_amount >actual_cost):
     profit = sale_amount - actual_cost
     print("Total profit = {0}".format(profit))
     
 else:
-    print("Oops you got loss")
+    print("Oops you got loss", loss)
 num = int(input("Please enter a number to compare whether it is greater than 15."))
 if(num  >15):
     print("The number is greater than 15")
