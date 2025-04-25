47a16007f339a4f2bb3cd2b8ec05ccbefc227e1f
Z-Coder10 <adetomiwaomololu@gmail.com>
2025-04-25 20:51:26 +0100

    turtle

---
+++ __pycache__/turtle.py
@@ -0,0 +1,19 @@
+import turtle
+
+turtle.Screen().bgcolor("turquoise")
+
+t = turtle.Turtle()
+
+t.forward(100)
+
+t.right(90)
+
+t.forward(100)
+
+t.right(90)
+
+t.forward(100)
+
+t.right(90)
+
+t.forward(100)
