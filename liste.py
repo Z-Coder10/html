f18ad55f90b980ed99314972b7d7e4c6648a018d
Z-Coder10 <adetomiwaomololu@gmail.com>
2025-05-21 19:46:48 +0100

    list

---
+++ liste.py
@@ -0,0 +1,19 @@
+my_list = [1,4,6,7,9,8,9,0,2,2]
+
+print(len(my_list))
+
+print(my_list[4])
+
+print(my_list[-1])
+
+my_list[4] = 10
+
+print(my_list)
+
+my_list.append(25)
+
+print(my_list)
+
+print(my_list.count(9))
+
+print(my_list.pop())
