39952cd27f4da2e33ba5b4e1fa3a1b128bfe2413
Z-Coder10 <adetomiwaomololu@gmail.com>
2025-05-29 20:41:42 +0100

    source

--- Dictionary.py
+++ Dictionary.py
@@ -1,4 +1,4 @@
-square = {1:1,3:9,5:25, 7:49, 9:81}
+square = {1:1,3:9,5:25,7:49, 9:81}
 for i in square:
     print(square[i])
     
---
+++ New.py
@@ -0,0 +1,21 @@
+# my_set= {1,3,6,6,3,9,0}
+# print(my_set)
+
+# st = {1,0, "David", (1,12,7)}
+# print(st)
+# myset = set([2,5,8,9,0,7,6,5])
+# print(myset)
+
+# num_set = set([0,1,5,7])
+# print("The Original set was ", num_set)
+ # num_set.pop()
+# print("After removing the first element from said set: ", num_set)
+
+setx = {"green,blue"}
+sety = {'blue', 'yellow'}
+print("Original set elements: ")
+print(setx)
+print(sety)
+print('Intersection of the two Elements')
+setz = setx.intersection(sety)
+print(setz)
